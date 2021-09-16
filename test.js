2b36c2faf39df9c4beb1748286f40de01265647b
zhouxiaoyue <zxy@zhouxiaoyue.cn>
2021-09-16 11:37:54 +0800

    0.1.0

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const browser_simulator = require('./index');
-const url = "https://m.tg3.net/kongbupian/56231/play-1-1.html";
+const url = "https://";
 (async function () {
     //实例化一个模拟器
     const executablePath = "/usr/bin/chromium-browser";
@@ -8,15 +8,17 @@ const url = "https://m.tg3.net/kongbupian/56231/play-1-1.html";
     await browser.launch({
         args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu'],
         // executablePath,
-        dumpio: true
+        dumpio: true,
+        headless: false
     });
     //打开指定网页
     let page;
     try {
-        page = await browser.open(url, [{
-            url: "/static/pingbi.js",
-            abort: true
-        }], null, 10, true);
+        // page = await browser.open(url, [{
+        //     url: "/static/pingbi.js",
+        //     abort: true
+        // }], null, 5000, true, true);
+        page = await browser.open(url, null, null, 100, true, true);
     } catch (e) {
         console.warn("打开网页异常：", e);
     } finally {
@@ -24,7 +26,8 @@ const url = "https://m.tg3.net/kongbupian/56231/play-1-1.html";
         page.completed && await page.close();
     }
     if (page && page.networks && page.networks.length) {
-        console.log("该网页的第一个请求", page.networks[0]);
+        // console.log("该网页的第一个请求", page.networks[0]);
+        console.log("共" + page.networks.length + "个请求");
     } else {
         console.warn("no request");
     }
